c3f93a69ec63b01fb8770a454867ba482692a185
Alexander Ivanov <alexandrbrown60@gmail.com>
2021-11-10 17:25:29 +0300

    Price and area diapasons

--- classes/CrmFinder.php
+++ classes/CrmFinder.php
@@ -38,31 +38,35 @@ class CrmFinder {
 	}
 
 	private function setParams($object) {
+			$areaTo = floatval($object->area) + 1;
+			$areaFrom = floatval($object->area) - 1;
+			$priceTo = intval($object->price) * 1.02;
+			$priceFrom = intval($object->price) * 0.98;
 
 			$flatFields = array(
 				array('id' => 667, 'value' => $object->street . " ул"), 
 				array('id' => 484, 'value' => $object->house),
 				array('id' => 446, 'value' => $object->rooms),
-				array('id' => 447, 'value' => $object->area),
+				array('id' => 447, 'value' => "$areaFrom & $areaTo"),
 				array('id' => 448, 'value' => $object->floor),
-				array('id' => 470, 'value' => $object->price),
+				array('id' => 470, 'value' => "$priceFrom & $priceTo"),
 				array('id' => 1522, 'value' => 'Наша база')
 			);
 			$houseFields = array(
 				array('id' => 1149, 'value' => $object->street . " ул"),
 				array('id' => 556, 'value' => $object->house),
 				array('id' => 530, 'value' => $object->rooms),
-				array('id' => 526, 'value' => $object->area),
+				array('id' => 526, 'value' => "$areaFrom & $areaTo"),
 				array('id' => 527, 'value' => $object->landArea),
-				array('id' => 528, 'value' => $object->price),
+				array('id' => 528, 'value' => "$priceFrom & $priceTo"),
 				array('id' => 1538, 'value' => 'Наша база')
 			);
 			$commercialFields = array(
 				array('id' => 668, 'value' => $object->street . " ул"),
 				array('id' => 515, 'value' => $object->house),
-				array('id' => 488, 'value' => $object->area),
+				array('id' => 488, 'value' => "$areaFrom & $areaTo"),
 				array('id' => 506, 'value' => $object->floor),
-				array('id' => 491, 'value' => $object->price),
+				array('id' => 491, 'value' => "$priceFrom & $priceTo"),
 				array('id' => 1530, 'value' => 'Наша база')
 			);
 
